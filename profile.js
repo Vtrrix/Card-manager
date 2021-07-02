a6b4d63fabb43c9ca27becdaf8876aff9fe4fbd2
Vivek <viveksonybcd@gmail.com>
2021-07-02 21:01:53 +0530

    cards fetch completed

--- profile.js
+++ profile.js
@@ -1,3 +1,5 @@
+let cardsDisplayArea = document.getElementById("cards");
+
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
@@ -15,29 +17,26 @@ function getCookie(cname) {
 }
 console.log(getCookie("userToken"));
 
-let clearCookie = () => {
+let logoutUser = () => {
+  location.replace("index.html");
   var allCookies = document.cookie.split(";");
   for (var i = 0; i < allCookies.length; i++)
     document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
   console.log("cookie cleared", document.cookie);
 };
-document.getElementById("clearCookie").addEventListener("click", clearCookie);
-document.getElementById("cookie").innerText = document.cookie;
+document.getElementById("logout").addEventListener("click", logoutUser);
 
 //Add and get cards request methods
 
 let AddCard = async () => {
   var myHeaders = new Headers();
-  myHeaders.append(
-    "Authorization",
-    "jwt eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MjQ4Nzg4MTksImlhdCI6MTYyNDg3ODUxOSwibmJmIjoxNjI0ODc4NTE5LCJpZGVudGl0eSI6M30.iMJ_gWR_JJgEI2C1d8dZXkkC2RvP85X-on1AIqq8aT8"
-  );
+  myHeaders.append("Authorization", `jwt ${getCookie("userToken")}`);
   myHeaders.append("Content-Type", "application/json");
 
   var raw = JSON.stringify({
     card_type: "debit",
-    card_no: 1111755,
-    cvv: 444,
+    card_no: 1121555,
+    cvv: 424,
     account_holder: "vtrix sony",
     phone_number: "9856471425",
   });
@@ -57,10 +56,7 @@ let AddCard = async () => {
 
 let GetCards = async () => {
   var myHeaders = new Headers();
-  myHeaders.append(
-    "Authorization",
-    "jwt eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MjQ4Nzg4MTksImlhdCI6MTYyNDg3ODUxOSwibmJmIjoxNjI0ODc4NTE5LCJpZGVudGl0eSI6M30.iMJ_gWR_JJgEI2C1d8dZXkkC2RvP85X-on1AIqq8aT8"
-  );
+  myHeaders.append("Authorization", `jwt ${getCookie("userToken")}`);
   myHeaders.append("Content-Type", "application/json");
 
   var requestOptions = {
@@ -70,7 +66,21 @@ let GetCards = async () => {
   };
 
   fetch("https://sql-injection-restapi.herokuapp.com/card/vtx", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      result.cards.map((card) => {
+        cardsDisplayArea.innerHTML += `
+          <div class="cardbox">
+            <div class="card">
+              <div class="card-front">${card.account_holder}</div>
+              <div class="card-back">${card.cvv}</div>
+            </div>
+          </div>
+        `;
+      });
+    })
     .catch((error) => console.log("error", error));
 };
+
+GetCards();
